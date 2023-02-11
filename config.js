07073bf5798318f8c995c2a9de3381d03c690f4b
muratcankolcu <92145567+muratcankolcu@users.noreply.github.com>
2023-02-11 21:25:46 +0300

    first

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "aslan1905",
+    database: "nodedb",
+  },
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const db = require("./data/db");
+
+const userRoutes = require("./routes/users");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
+
+app.use(userRoutes);
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
